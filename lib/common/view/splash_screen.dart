263916c8b24a797ab580a2592ea0ffe83ffcaaa3
pyowonsik <qqrtyu@gmail.com>
2023-09-20 08:40:55 +0900

    feat : refresh 토큰 만료시 재로그인 구현

--- lib/common/view/splash_screen.dart
+++ lib/common/view/splash_screen.dart
@@ -3,6 +3,7 @@ import 'package:delivery_app/common/const/data.dart';
 import 'package:delivery_app/common/layout/default_layout.dart';
 import 'package:delivery_app/common/view/root_tab.dart';
 import 'package:delivery_app/user/view/login_screen.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -23,14 +24,22 @@ class _SplashScreenState extends State<SplashScreen> {
   void checkToken() async {
     final refreshToken = await storage.read(key: REFRESH_TOKEN_KEY);
     final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+    final dio = Dio();
 
-    if (refreshToken == null || accessToken == null) {
+    // Refresh Token으로 Access Token 발급
+    // Refresh Token 만료시 자동로그인 안됨 , 다시 로그인 페이지
+    try {
+      final resp = await dio.post(
+        'http://$ip/auth/token',
+        options: Options(headers: {'authorization': 'Bearer $refreshToken'}),
+      );
       Navigator.of(context).pushAndRemoveUntil(
-          MaterialPageRoute(builder: (context) => LoginScreen()),
+          MaterialPageRoute(builder: (context) => const RootTab()),
           (route) => false);
-    } else {
+    } catch (e) {
       Navigator.of(context).pushAndRemoveUntil(
-          MaterialPageRoute(builder: (context) => RootTab()), (route) => false);
+          MaterialPageRoute(builder: (context) => const LoginScreen()),
+          (route) => false);
     }
   }
 
