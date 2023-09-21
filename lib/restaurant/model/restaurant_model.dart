6342610595c463fff9029b5d974295d9434901d1
pyowonsik <qqrtyu@gmail.com>
2023-09-21 09:21:19 +0900

    feat : Restaurant 모델

---
+++ lib/restaurant/model/restaurant_model.dart
@@ -0,0 +1,25 @@
+class RestaurantModel {
+  final String id;
+  final String name;
+  final String thumbUrl;
+  final List<String> tags;
+  final RestaurantPriceRange priceRange;
+  final double ratings;
+  final int ratingsCount;
+  final int deliveryTime;
+  final int deliveryFee;
+
+  const RestaurantModel({
+    required this.id,
+    required this.name,
+    required this.thumbUrl,
+    required this.tags,
+    required this.priceRange,
+    required this.ratings,
+    required this.ratingsCount,
+    required this.deliveryTime,
+    required this.deliveryFee,
+  });
+}
+
+enum RestaurantPriceRange { expensive, medium, cheap }
--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -1,5 +1,6 @@
 import 'package:delivery_app/common/const/data.dart';
 import 'package:delivery_app/restaurant/component/restaurant_cart.dart';
+import 'package:delivery_app/restaurant/model/restaurant_model.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
@@ -22,43 +23,51 @@ class RestaurantScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: FutureBuilder<List>(
-            future: paginateRestaurant(),
-            builder: ((context, AsyncSnapshot<List> snapshot) {
-              if (!snapshot.hasData) {
-                return Container();
-              }
-
-              return ListView.separated(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  final item = snapshot.data![index];
-
-                  return RestaurantCard(
-                    image: Image.network(
-                      'http://$ip${item['thumbUrl']}',
-                      fit: BoxFit.cover,
-                    ),
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: FutureBuilder<List>(
+          future: paginateRestaurant(),
+          builder: ((context, AsyncSnapshot<List> snapshot) {
+            if (!snapshot.hasData) {
+              return Container();
+            }
+            return ListView.separated(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final item = snapshot.data![index];
+                final pItem = RestaurantModel(
+                    id: item['id'],
                     name: item['name'],
-                    tags: List<String>.from(['tags']),
+                    thumbUrl: 'http://$ip${item['thumbUrl']}',
+                    tags: List<String>.from(item['tags']),
+                    priceRange: RestaurantPriceRange.values
+                        .firstWhere((e) => e.name == item['priceRange']),
+                    ratings: item['ratings'],
                     ratingsCount: item['ratingsCount'],
                     deliveryTime: item['deliveryTime'],
-                    deliveryFee: item['deliveryFee'],
-                    ratings: item['ratings'],
-                  );
-                },
-                separatorBuilder: (context, index) {
-                  return const SizedBox(
-                    height: 16.0,
-                  );
-                },
-              );
-            }),
-          ),
+                    deliveryFee: item['deliveryFee']);
+
+                return RestaurantCard(
+                  image: Image.network(
+                    pItem.thumbUrl,
+                    fit: BoxFit.cover,
+                  ),
+                  name: pItem.name,
+                  tags: pItem.tags,
+                  ratingsCount: pItem.ratingsCount,
+                  deliveryTime: pItem.deliveryTime,
+                  deliveryFee: pItem.deliveryFee,
+                  ratings: pItem.ratings,
+                );
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox(
+                  height: 16.0,
+                );
+              },
+            );
+          }),
         ),
       ),
     );
